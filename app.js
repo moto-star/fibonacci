ada07a2887775554b585393c05487e3c7553f574
moto-star <aruimi.okasii@gmail.com>
2019-09-09 20:38:27 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 'use strict';
+var fibMap = new Map();
+fibMap.set(0, 0);
+fibMap.set(1, 1);
+function fib(n) {
+    if (fibMap.has(n)) {
+        return fibMap.get(n);
+    }
+    const value = fib(n-1) + fib(n-2);
+    fibMap.set(n,value);
+    return value;
+}
+console.log(fib(0));
+console.log(fib(1));
+console.log(fib(2));
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
